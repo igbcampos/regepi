68f5ae465920252525a0e34be35fc2a78a4b1283
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-10-13 20:22:24 -0300

    Adiciona fotos de novos participantes

--- regepi/settings.py
+++ regepi/settings.py
@@ -71,7 +71,6 @@ DATABASES = {
     #     'HOST': '127.0.0.1',
     #     'PORT': '5432',
     # }
-
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
